522f02352460c1a94544a9722e7dd9c31d87f704
Your Name <you@example.com>
2020-05-28 11:45:53 +0800

    changep

--- src/main/java/net/fpeg/msa/wordbase/service/WordUserService.java
+++ src/main/java/net/fpeg/msa/wordbase/service/WordUserService.java
@@ -1,28 +1,40 @@
 package net.fpeg.msa.wordbase.service;
 
+import net.fpeg.msa.common.utils.MvcUtil;
+import net.fpeg.msa.wordbase.dao.WordTagDao;
 import net.fpeg.msa.wordbase.dao.WordUserDao;
 import net.fpeg.msa.wordbase.entity.WordSource;
+import net.fpeg.msa.wordbase.entity.WordTag;
 import net.fpeg.msa.wordbase.entity.WordUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
 
 @Service
 public class WordUserService {
     @Autowired
     WordUserDao wordUserDao;
 
+    @Autowired
+    WordTagDao wordTagDao;
+
     public void register(Long userId) {
-        if(wordUserDao.countByWordUserId(userId)==0)
-        {
+        if (wordUserDao.countByWordUserId(userId) == 0) {
             //级联保存
             WordUser wordUser = WordUser.builder()
                     .wordUserId(userId)
                     .build();
+            WordTag wordTag = MvcUtil.insertIfNotExist("默认", "wordTagValue", wordTagDao::getByWordTagValue, wordTagDao::save, WordTag.class);
+            wordTag.getWordUsers().add(wordUser);
+            HashSet<WordTag> wordTags = new HashSet<>(Collections.singletonList(wordTag));
             WordSource wordSource = WordSource.builder()
                     .wordUser(wordUser)
                     .wordSourceValue("根节点")
                     .build();
             wordUser.setWordSource(wordSource);
+            wordUser.setWordTags(wordTags);
             wordUserDao.save(wordUser);
         }
     }
